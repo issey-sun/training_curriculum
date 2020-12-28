91c97e968477255f50047f5a4eb2459ff2ebe95b
issey-sun <76093901+issey-sun@users.noreply.github.com>
2020-12-28 12:33:31 +0900

    ISSUE1

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -34,7 +34,8 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
+      # Issue1を解消。変更内容はハッシュロケット→シンボル型とし、よりシンプルな記述に変更
       @week_days.push(days)
     end
 
